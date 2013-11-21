0d6fd54a44ee56d11718f3ef255e954216c2a4b3
Nishita Shukla <nishitashukla@Nishitas-Mac-mini.local>
2013-11-21 13:15:17 +0530

    Initial Commit

---
+++ ImageSlider/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ImageSlider
+//
+//  Created by Nishita Shukla on 21/11/13.
+//  Copyright (c) 2013 Nishita Shukla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ImageSlider/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ImageSlider
+//
+//  Created by Nishita Shukla on 21/11/13.
+//  Copyright (c) 2013 Nishita Shukla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
